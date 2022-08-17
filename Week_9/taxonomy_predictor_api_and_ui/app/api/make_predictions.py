733eb9c3bc71cc849ee586709542ab96a7f58b36
venkteshv <venkteshv@venkteshvs-MacBook-Air.local>
2022-08-17 23:03:33 +0530

    added bulk upload logic

--- Week_9/taxonomy_predictor_api_and_ui/app/api/make_predictions.py
+++ Week_9/taxonomy_predictor_api_and_ui/app/api/make_predictions.py
@@ -9,29 +9,99 @@ from sentence_transformers import SentenceTransformer
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 sent_model = SentenceTransformer('bert-large-nli-stsb-mean-tokens')
-model_path_recommend = os.path.join(dir_path, '../models/model_euclidean_SENT_BERT_cos_1')
+model_path_recommend = os.path.join(dir_path, '../models/model_euclidean_SENT_BERT_cos_attention_2_V3')
 pathData = os.path.join(dir_path, '../data')
 
 test_labels = joblib.load(pathData+'/test_labels')
 test_labels = np.array(test_labels)
 
+train_labels = joblib.load(pathData+'/train_labels')
+train_labels = np.array(train_labels)
+
 LE = joblib.load(pathData+'/label_encoder_reduced')
+
+cos_label = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+
 
 labels_with_board = joblib.load(pathData+'/labels_formatted')
 
+class MHSA(nn.Module):
+  def __init__(self,
+         emb_dim,
+         kqv_dim,
+         num_heads=2):
+    super(MHSA, self).__init__()
+    self.emb_dim = emb_dim
+    self.kqv_dim = kqv_dim
+    self.num_heads = num_heads
+
+    self.w_k = nn.Linear(emb_dim, kqv_dim * num_heads, bias=False)
+    self.w_q = nn.Linear(emb_dim, kqv_dim * num_heads, bias=False)
+    self.w_v = nn.Linear(emb_dim, kqv_dim * num_heads, bias=False)
+    self.w_out = nn.Linear(kqv_dim * num_heads, emb_dim)
+
+  def forward(self, query, key, value):
+    # print("query",query.shape)
+    b, t = query.shape
+    e = self.kqv_dim
+    h = self.num_heads
+    keys = self.w_k(key).view(b, h, e)
+    values = self.w_v(value).view(b, h, e)
+    queries = self.w_q(query).view(b, h, e)
+
+    # keys = keys.transpose(2, 1)
+    # queries = queries.transpose(2, 1)
+    # values = values.transpose(2, 1)
+
+    dot = queries @ keys.transpose(2, 1)  #(b*h*e) @ (b*e*h)
+    dot = dot / np.sqrt(e)  # (b*h*h)
+    dot = F.softmax(dot, dim=2)
+
+    out = dot @ values   # (b*h*h) @ (b*h*e) = (b*h*e)
+    out = out.contiguous().view(b, h * e)
+    out = self.w_out(out)
+    return out
+    
 class MulticlassClassifier(nn.Module):
     def __init__(self,bert_model_path):
         super(MulticlassClassifier,self).__init__()
-        self.bert = BertModel.from_pretrained(bert_model_path,output_hidden_states=False,output_attentions=False)
+        self.bert = BertModel.from_pretrained(bert_model_path,output_hidden_states=True,output_attentions=False)
         self.dropout = nn.Dropout(0.1)
-        self.fc1 = nn.Linear(768, 384)
-        self.fc2 = nn.Linear(384, 1024)
+        self.fc1 = nn.Linear(768, 1024)
+        self.fc2 = nn.Linear(576, 1024)
+        self.fc3 = nn.Linear(1024, 512)
+        self.act = torch.nn.ReLU()
+        self.fc4 = nn.Linear(512, 1024)
 
-    def forward(self,tokens,masks):
-        _, pooled_output = self.bert(tokens, attention_mask=masks)
+        self.multi_head_attention = MHSA(1024, 64,16)
+        self.multihead_attn = torch.nn.MultiheadAttention(embed_dim = 1024,  num_heads = 4, batch_first=True)
+
+
+    def forward(self,tokens,masks, targets=None, skip_attention=False):
+        outputs = self.bert(tokens, attention_mask=masks)[2]
+        output_1 = outputs[-1].permute(1,0,2)
+        output_1 = torch.mean(output_1, dim=0)
+        pooled_output = outputs[-1] # output_1 # torch.cat((output_1, output_2), dim=1)
         x = self.fc1(pooled_output)
-        x = self.fc2(x)
+        # x = self.fc2(x)
+        
+        targets_curr_batch = []
+        for index_1, input_x in enumerate(x):
+            # print(input_x.shape, torch.mean(input_x,dim=0).shape)
+            distance = cos_label(torch.mean(input_x,dim=0).reshape(1,-1), unique_poincare_tensor)
+            distances,indices = torch.topk(distance,1,largest=True)
+
+            target_distances = (F.normalize(unique_poincare_tensor[indices],p=2,dim=1) - F.normalize(unique_poincare_tensor,p=2,dim=1)).pow(2).sum(1) #cos_label(unique_poincare_tensor[indices].reshape(1,-1), unique_poincare_tensor)
+            distances,indices = torch.topk(target_distances,5,largest=False)
+            targets_curr_batch.append(unique_poincare_tensor[indices].reshape(1,5,1024))
+
+        targets_batch = torch.cat(targets_curr_batch, dim=0)
+
+        attn_output, attn_output_weights = self.multihead_attn(targets_batch, x, x)
+
+        x = torch.sum(attn_output,dim=1)
         return x
+
 
 
 model = MulticlassClassifier('bert-base-uncased')
@@ -50,13 +120,14 @@ def get_cleaned_taxonomy(taxonomy):
   return cleaned_taxonomy
 
 
-def get_taxonomy_embeddings():
-    cleaned_taxonomy = get_cleaned_taxonomy(test_labels)
+def get_taxonomy_embeddings(labels):
+    cleaned_taxonomy = get_cleaned_taxonomy(labels)
     taxonomy_vectors = sent_model.encode(cleaned_taxonomy)
     taxonomy_vectors = np.vstack(taxonomy_vectors)
     test_poincare_tensor = torch.tensor(taxonomy_vectors,dtype=torch.float)
     return test_poincare_tensor
-test_poincare_tensor = get_taxonomy_embeddings()
+test_poincare_tensor = get_taxonomy_embeddings(test_labels)
+unique_poincare_tensor = get_taxonomy_embeddings(train_labels)
 print("test_labels",test_poincare_tensor.shape)
 
 cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
@@ -97,10 +168,10 @@ def recommend_taxonomy(text):
         for formatted_label in labels_with_board:
             if label in formatted_label and len(label.split(">>"))>1:
                 final_list.append((formatted_label,distance))
-    for (prediction,distance)  in final_list:
+    for (prediction,distance) in final_list:
         results.append({
-            "taxonomy": prediction,
-            "confidence": distance       })
+            "taxonomy": prediction  ,
+            "confidence": distance      })
 
 
     return results
--- Week_9/taxonomy_predictor_api_and_ui/app/api/taxonomy_prediction.py
+++ Week_9/taxonomy_predictor_api_and_ui/app/api/taxonomy_prediction.py
@@ -1,6 +1,9 @@
 from typing import List
-from fastapi import Header, APIRouter
+import csv
+import codecs
+from fastapi import Header, APIRouter, UploadFile, File, Request
 from app.api.models import Taxonomies
+import pandas as pd
 from app.api.models import LearningContent
 from app.api.make_predictions import recommend_taxonomy
 
@@ -13,8 +16,13 @@ async def get_predictions(payload: LearningContent):
 
 
 @taxonomy_predictor.post('/gettaxonomy/batch',response_model=List[List[Taxonomies]])
-async def get_predictions(payload: List[LearningContent]):
+async def get_predictions(payload: Request):
     results = []
-    for learning_content in payload:
-         results.append(recommend_taxonomy(learning_content.content))
+    file = await payload.form()
+    print(file["file"].file)
+    csvReadContent = pd.read_csv(file["file"].file,header=None)
+    print("csvReadContent",csvReadContent)
+    for ques, learning_content in csvReadContent.values:
+        print("ques",ques)
+        results.append(recommend_taxonomy(ques))
     return results
